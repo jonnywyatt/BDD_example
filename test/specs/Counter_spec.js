2b84ea2e1764c9b3d84919f674d5c3313f6276fb
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-07-14 21:05:21 +0100

    Empty Counter tests

--- test/specs/Counter_spec.js
+++ test/specs/Counter_spec.js
@@ -11,22 +11,26 @@ describe('Counter', function () {
           self.module = new Mod(self.$html);
           self.$count = self.$html.find('.counter__count');
           self.$add = self.$html.find('.counter__add');
-          self.$remove = self.$html.find('.counter__count');
+          self.$remove = self.$html.find('.counter__remove');
+          self.$history = self.$html.find('.counter__history');
+          self.$historyValues = self.$html.find('.counter__history-values');
           done();
         }, done);
   });
 
-  function isOpen($el) {
-    return $el.hasClass('active');
-  }
 
-  describe('toggle panel', function () {
-
-    it('should open when user clicks the open button', function () {
-    });
-
+  xit('should increase the count when the user clicks Add', function () {
 
   });
+
+  xit('should decrease the count when the user clicks Remove', function () {
+
+  });
+
+  xit('should populate the history with previous count values', function () {
+
+  });
+
 
 });
 
