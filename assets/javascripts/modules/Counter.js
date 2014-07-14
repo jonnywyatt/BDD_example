fceab29f3f6389b3cb9331c64dcda16b0d3bbbbd
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-07-14 21:06:57 +0100

    Filled Counter implementation

--- assets/javascripts/modules/Counter.js
+++ assets/javascripts/modules/Counter.js
@@ -3,6 +3,22 @@ define([], function () {
   'use strict';
 
   var Counter = function ($el) {
+    var $add = $el.find('.counter__add'),
+        $remove = $el.find('.counter__remove'),
+        currentCount = parseInt($el.find('.counter__count').text(), 10),
+        $historyValues = $el.find('.counter__history-values');
+
+    $add.on('click', function() {
+      currentCount++;
+      $historyValues.text($historyValues.text() + ', ' + currentCount);
+      $el.find('.counter__count').text(currentCount);
+    });
+
+    $remove.on('click', function() {
+      currentCount--;
+      $historyValues.text($historyValues.text() + ', ' + currentCount);
+      $el.find('.counter__count').text(currentCount);
+    });
 
   };
 
